87aeda907d20f7899239f016a38c45264d4b5836
StheshSphere <stheshsphere@gmail.com>
2026-04-12 23:36:02 +0200

    delete

--- shared-auth-foundation/src/js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabaseUrl = 'https://sqbscxfolbckikrzxqhr.supabase.co'
-const supabaseKey = 'sb_publishable_Zw_iCK1n54xXGPuDWALWQQ_k2cOQWay'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
